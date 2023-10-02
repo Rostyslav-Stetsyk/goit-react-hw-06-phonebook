be44d99ed6db53ae508d6d080d636629719a3594
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2023-10-02 16:27:57 +0300

    completed

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { ContactListStyled, ListElement } from './ContactList.styled';
+import { deleteContact } from 'redux/contactSlice';
+import { getContacts, getFilter } from 'redux/selectors';
 
-export const ContactList = ({ contacts, onDelete }) => {
+export const ContactList = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const dispach = useDispatch();
+
   return (
     <ContactListStyled>
-      {contacts.map(({ id, name, number }) => (
-        <ListElement key={id}>
-          <p>
-            {name}: {number}
-          </p>
-          <button type="button" id={id} onClick={e => onDelete(e.target.id)}>
-            Delete
-          </button>
-        </ListElement>
-      ))}
+      {contacts
+        .filter(el => el.name.toLowerCase().includes(filter))
+        .map(({ id, name, number }) => (
+          <ListElement key={id}>
+            <p>
+              {name}: {number}
+            </p>
+            <button
+              type="button"
+              id={id}
+              onClick={e => dispach(deleteContact(e.target.id))}
+            >
+              Delete
+            </button>
+          </ListElement>
+        ))}
     </ContactListStyled>
   );
 };
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,17 @@
+import { useDispatch } from 'react-redux';
 import { FilterForm } from './Filter.styled';
+import { setFilter } from 'redux/filterSlice';
 
-export const Filter = ({ onChange }) => {
+export const Filter = () => {
+  const dispatch = useDispatch();
   return (
     <FilterForm>
       <label htmlFor="find">Find contacts by name:</label>
-      <input type="text" name="find" onChange={e => onChange(e.target.value)} />
+      <input
+        type="text"
+        name="find"
+        onChange={e => dispatch(setFilter(e.target.value.toLowerCase()))}
+      />
     </FilterForm>
   );
 };
